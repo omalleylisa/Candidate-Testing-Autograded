8db10a37fb3f584b2e1add97885eb89e8c8338e5
Lisa O'Malley <lisaomalley@Lisas-MBP.lan>
2024-01-11 01:03:47 -0600

    assignment 1 part 1 third try

--- candidate-testing.js
+++ candidate-testing.js
@@ -4,11 +4,11 @@ const input = require('readline-sync');
 
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
-
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
+
 
 //TODO: Variables for Part 2
 let questions;
@@ -24,16 +24,19 @@ function askForName() {
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   candidateAnswer = input.question(question);
+
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+ 
   if (candidateAnswer === correctAnswer) {
-  console.log("Correct! The answer is Sally Ride.");
-  } else {
-    console.log("Incorrect answer.");
-  }
+    console.log("Correct! The answer is Sally Ride.");
+    } else {
+      console.log("Incorrect answer.");
+    }
+  
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
 
@@ -43,7 +46,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log("Hello, " + candidateName + "!");
+   console.log("Hello, " + candidateName);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
