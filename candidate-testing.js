b2d89b814aefceb55d53431e598e3839226152a6
Lisa O'Malley <lisaomalley@Lisas-MBP.lan>
2024-01-11 18:40:45 -0600

    graded assignment part 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -18,7 +18,7 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  candidateName = input.question("Enter your name: ");
+  candidateName = input.question("Please enter your name: ");
 }
 
 function askQuestion() {
